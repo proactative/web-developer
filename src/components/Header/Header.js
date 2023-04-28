a303216f81ca318b519cd1a3d9b5020b7a478f09
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-04-28 08:55:20 +0300

    feat: add fixed header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,31 @@
 import React from 'react'
-import './Header.css';
+import './Header.css'
 
 function Header() {
   return (
-    <div className="header">
-      <div className='header__me'>
-        <div className="header__photo" />
-        <h2>Tatiana Parfeniuk</h2>
-      </div>
-      <div className="header__lang-toggle">
-        <button className='header__btn' type="button">En</button>
-        <button className='header__btn' type="button">Ru</button>
-      </div>
+    <header className="header">
+      <div className="header__container">
+        <div className="header__me">
+          <div className="header__photo" />
+          <h2>Tatiana Parfeniuk</h2>
+        </div>
 
-      
-    </div>
+        <ul className='header__links'>
+          <p>Skills</p>
+          <p>Portfolio</p>
+          <p>Contacts</p>
+        </ul>
+
+        <div className="header__lang-toggle">
+          <button className="header__btn" type="button">
+            En
+          </button>
+          <button className="header__btn" type="button">
+            Ru
+          </button>
+        </div>
+      </div>
+    </header>
   )
 }
 
