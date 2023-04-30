f7cc3ba4f6a2fd816fde58b0e01a2d73a7ad010d
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-04-30 18:30:27 +0300

    feat: add sticky footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,51 @@
 import React from 'react'
-import './Footer.css';
+import './Footer.css'
 
 function Footer() {
   return (
-    <div className="footer">
-        <ul className="contacts">
-            <li><p>email</p></li>
-            <li><p>telephone</p></li>
-            <li><p>telegram</p></li>
-        </ul> 
-        {/* <p>2023 @copy;Tatiana Parfeniuk</p> */}
-    </div>
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__main">
+          <p className="footer__invitation">Feel free to write to me</p>
+          <ul className="footer__contacts">
+            <li>
+              <a
+                href="mailto:tatiana.vik@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+                className="footer__contact-item"
+              >
+                <span className="footer__contact-icon footer__contact-icon_email" />
+                <p className="footer__contact-text">email@gmail.com</p>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/proactative"
+                target="_blank"
+                rel="noreferrer"
+                className="footer__contact-item"
+              >
+                <span className="footer__contact-icon footer__contact-icon_github" />
+                <p className="footer__contact-text">github</p>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://t.me/proactative"
+                target="_blank"
+                rel="noreferrer"
+                className="footer__contact-item"
+              >
+                <span className="footer__contact-icon footer__contact-icon_telegram" />
+                <p className="footer__contact-text">@proactative</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <p className="footer__copyright">&copy;Tatiana Parfeniuk 2023</p>
+      </div>
+    </footer>
   )
 }
 
