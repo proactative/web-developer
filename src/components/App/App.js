4f3cf3c0c145a6ac72980e2b978d0051ede9e7c0
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-05-03 12:46:35 +0300

    feat: add translation functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,54 @@
-import './App.css';
-import Header from '../Header/Header';
-import Intro from '../Intro/Intro';
-import Portfolio from '../Portfolio/Portfolio';
-import Footer from '../Footer/Footer';
+import React from 'react'
+import { useState, useEffect } from 'react'
+import i18next from 'i18next';
+
+import './App.css'
+import Header from '../Header/Header'
+import Intro from '../Intro/Intro'
+import Skills from '../Skills/Skills'
+import Portfolio from '../Portfolio/Portfolio'
+import Footer from '../Footer/Footer'
+
 
 function App() {
+  const [hiddenSectionsSeen, setHiddenSectionsSeen] = useState(false)
+
+ const showHiddenSections = () => {
+    setHiddenSectionsSeen(true)
+  }
+
+  /* translation functionality */
+  useEffect(() => {
+    sessionStorage.setItem('i18nextLng', 'en')
+  }, [])
+
+  const setLanguageEN = () => {
+    sessionStorage.setItem('i18nextLng', 'en')
+    i18next.changeLanguage("en");
+  }
+
+  const setLanguageRU = () => {
+    sessionStorage.setItem('i18nextLng', 'ru')
+    i18next.changeLanguage("ru");
+  }
+
   return (
     <div className="app">
-      <Header />
-      <Intro />
-      <Portfolio />
+      <Header setLanguageEN={setLanguageEN} setLanguageRU={setLanguageRU} />
+      <main className="app__main">
+        <Intro showHiddenSections={showHiddenSections} />
+        <div
+          className={
+            hiddenSectionsSeen ? 'app__more' : 'app__more app__more_hidden'
+          }
+        >
+          <Skills />
+          <Portfolio />
+        </div>
+      </main>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
