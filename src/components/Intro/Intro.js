4f2180f071e83b8a49434276981a01d9f23e96f9
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-05-05 09:03:55 +0300

    feat: add adaptive-responsive layout of 'Intro' section

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,16 +1,37 @@
 import React from 'react'
-import './Intro.css';
+import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import './Intro.css'
 
 function Intro() {
+  const { t } = useTranslation()
+  const [title, setTitle] = useState('WEB')
+  const text = '--DEVELOPER_'
+  let currentIndex = 0
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTitle((prev) => prev + text[currentIndex])
+      currentIndex += 1
+      let final = text.length - 1
+      if (currentIndex === final) {
+        clearInterval(id)
+      }
+    }, 80)
+    return () => clearInterval(id)
+  }, [])
+
   return (
-    <div className="intro">
-        <h1 className='intro__title'>Web-developer</h1>
-        <div className="intro__text">lorem lorem lorem lorem</div>
-        <div className='intro__popups'>
-          <p>My skills</p>
-          <p>IT education</p>
-        </div>
-    </div>
+    <section className="intro">
+      <div className="intro__container">
+        <h1 className="intro__title">{title}</h1>
+        <button className="intro__button" type="button">
+          {t('more')}
+        </button>
+      </div>
+      <div className="intro__decor" />
+    </section>
   )
 }
 
