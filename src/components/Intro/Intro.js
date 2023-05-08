5fd3da21a5fdbb390925e3add4080c69b7c04156
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-05-08 16:10:08 +0300

    feat: enhance Intro section (adaptive-responsive layout, title animation, button appear with delay)

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,30 +4,66 @@ import { useTranslation } from 'react-i18next'
 
 import './Intro.css'
 
-function Intro() {
+function Intro(props) {
   const { t } = useTranslation()
-  const [title, setTitle] = useState('WEB')
-  const text = '--DEVELOPER_'
-  let currentIndex = 0
+  const [visibleButton, setVisibleButton] = useState(false)
+ 
+  const showButton = () => {
+    setVisibleButton(true)
+  }
 
   useEffect(() => {
-    const id = setInterval(() => {
-      setTitle((prev) => prev + text[currentIndex])
-      currentIndex += 1
-      let final = text.length - 1
-      if (currentIndex === final) {
-        clearInterval(id)
-      }
-    }, 80)
-    return () => clearInterval(id)
+    setTimeout(showButton, 2500)
   }, [])
 
   return (
     <section className="intro">
       <div className="intro__container">
-        <h1 className="intro__title">{title}</h1>
-        <button className="intro__button" type="button">
-          {t('more')}
+        <h1 className="intro__title">
+          WEB
+          <span className="l1">
+            -
+            <span className="l2">
+              D
+              <span className="l3">
+                E
+                <span className="l4">
+                  V
+                  <span className="l5">
+                    E
+                    <span className="l6">
+                      L
+                      <span className="l7">
+                        O
+                        <span className="l8">
+                          P 
+                          <span className="l9">
+                            E
+                            <span className="l10">
+                              R
+                              <span className="l11">_</span>
+                            </span>
+                          </span>
+                        </span>
+                      </span>
+                    </span>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        </h1>
+        <button
+          onClick={props.getStarted}
+          className={
+            visibleButton
+              ? 'intro__button'
+              : 'intro__button intro__button_invisible'
+          }
+        >
+          <a className="intro__more-link" href="#aboutMe">
+            {t('more')}
+          </a>
         </button>
       </div>
       <div className="intro__decor" />
